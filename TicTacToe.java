272f8b248eb277deb2e530b636ac5cde750e6aa3
sweetylenka <sweetylenka2013@gmail.com>
2021-03-31 12:09:07 +0530

    As a Player would like to start fresh by creating a tic tac toe board - Create a TicTacToeGame class - Create method for every use case and call from main - Create a board of char[] of size 10 and assign empty space to each element
    -
    0th index is ignored to make it
    
    UC 1 user friendly

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,38 @@
 package tictactoe;
 import java.util.Scanner;
-public class TicTacToe {
 
+public class TicTacToe {
+	 
+	
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("WELCOME TO TIC-TAC-TOE GAME");
+		System.out.println("WELCOME TO TIC-TAC-TOE GAME\n");
+		
+		char[][] Board = {{' ',' ',' '},
+						  {' ',' ',' '},
+						  {' ',' ',' '}};
+		
+		
+		displayBoard(Board);
+		
+		
+		
+				
+		}
+
+	private static void displayBoard(char[][] Board) {
+		System.out.println(Board[0][0]+ "|" + Board[0][1]+ "|" +Board[0][2]);
+		System.out.println("-+-+-");
+		System.out.println(Board[1][0]+ "|" + Board[1][1]+ "|" +Board[1][2]);
+		System.out.println("-+-+-");
+		System.out.println(Board[2][0]+ "|" + Board[2][1]+ "|" +Board[2][2]);
 	}
-}
+				
+		
+		
+	}
+
 
 
