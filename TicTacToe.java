4ddb6922c1ef8cc8b67848f7f660c467e47bf4fe
sweetylenka <sweetylenka2013@gmail.com>
2021-03-31 10:50:48 +0530

    WELCOME TO TIC-TAC-TOE GAME

--- TicTacToe.java
+++ TicTacToe.java
@@ -5,60 +5,7 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("WELCOME TO TIC-TAC-TOE GAME");
-		char [][] gameBoard = {{' ','|',' ','|',' '},
-				{' ','+','-','+','-'},
-				{' ','|',' ','|',' '},
-				{'-','+','-','+','-'},
-				{' ','|',' ','|',' '}};
-		
-		printGameBoard(gameBoard);
-		 allowPlayerToChooseXorO();
-		
 	}
-		
-		
-		
-		
-		public static void printGameBoard(char[][] gameBoard) {
-			for(char[] row : gameBoard) {
-				for(char c : row) {
-					System.out.print(c);
-				}
-				System.out.println();
-			}
-		}
-		
-	
-		public static void allowPlayerToChooseXorO()
-		{
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Choose X or O");
-			String input=sc.nextLine();
-			char player = input.charAt(0);
-			char computer;
-			if (player != 'X' && player != 'O' )
-			{
-				System.out.println("Choose either X or O ");
-				
-			}
-			if(player == 'X')
-			{
-				computer='O';
-				System.out.println("Player has chosen :"+player);
-				System.out.println("Computer :"+computer);
-			}
-			else
-			{
-				computer='X';
-				System.out.println("Computer : "+computer);
-				System.out.println("Player :"+player);
-			}
-			
-				
-			
-		}
-
-
-	}
+}
 
 
