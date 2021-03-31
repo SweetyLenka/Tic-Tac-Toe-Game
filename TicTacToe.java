94daed395061929d8f50e974cd8b1b12518274e1
sweetylenka <sweetylenka2013@gmail.com>
2021-03-31 18:42:22 +0530

    UC5 - Ability to check for the free space before making the desired move

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,10 @@
 package tictactoe;
 import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe {
 	 public static char player1;
+	 public static char computer;
 
 
 	public static void main(String[] args) {
@@ -18,16 +20,32 @@ public class TicTacToe {
 		allowPlayerToChooseEitherXorO();
 		abiltytoMovetoDesiredLocation(Board);
 		showBoard(Board);
-		
+		computerTurn(Board);
+	}
+	
+	private static void computerTurn(char[][] Board) {
+		Random rand = new Random();
+		int computerMove;
+		while (true) {
+			computerMove = rand.nextInt(9) + 1;
+			if (isValidMove(Board,computerMove)) {
+				break;
+			}
 		}
-				
+		System.out.println("Computer chose location : " + computerMove);
+		placeMove(Board,Integer.toString(computerMove),computer);
+		showBoard(Board);
+	}
+
+		
+	
+	
 	public static void allowPlayerToChooseEitherXorO()
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Choose either X or O :");
 		String userInput = sc.nextLine();
 		player1=userInput.charAt(0);
-		char computer;
 		if (player1 != 'X' && player1 != 'O' && player1 != 'x' && player1 != 'o' )
 		{
 			System.out.println("Please enter a valid choice (Enter either X or O) : ");
@@ -68,40 +86,69 @@ public class TicTacToe {
 		Scanner scanner = new Scanner(System.in);
 		String desiredLocation=scanner.nextLine();
 		System.out.println("Loaction Selected :"+desiredLocation);
+		placeMove(Board, desiredLocation,player1);
+	}
+
+	private static void placeMove(char[][] Board, String desiredLocation,char letter) {
 		switch(desiredLocation) {
 		case "1":
-			Board[0][0]=player1;
+			Board[0][0]=letter;
 			break;
 		case "2":
-			Board[0][1]=player1;
+			Board[0][1]=letter;
 			break;
 		case "3":
-			Board[0][2]=player1;
+			Board[0][2]=letter;
 			break;
 		case "4":
-			Board[1][0]=player1;
+			Board[1][0]=letter;
 			break;
 		case "5":
-			Board[1][1]=player1;
+			Board[1][1]=letter;
 			break;
 		case "6":
-			Board[1][2]=player1;
+			Board[1][2]=letter;
 			break;
 		case "7":
-			Board[2][0]=player1;
+			Board[2][0]=letter;
 			break;
 		case "8":
-			Board[2][1]=player1;
+			Board[2][1]=letter;
 			break;
 		case "9":
-			Board[2][2]=player1;
+			Board[2][2]=letter;
 			break;
 		default:
 			System.out.println("Choose a location between (1-9)");
 		
 		}
 	}
-			
+		private static boolean isValidMove (char[][] Board, int position) {
+			switch(position) {
+				case 1:
+					return (Board[0][0] == ' ');
+				case 2:
+					return (Board[0][1] == ' ');
+				case 3:
+					return (Board[0][2] == ' ');
+				case 4:
+					return (Board[1][0] == ' ');
+				case 5:
+					return (Board[1][1] == ' ');
+				case 6:
+					return (Board[1][2] == ' ');
+				case 7:
+					return (Board[2][0] == ' ');
+				case 8:
+					return (Board[2][1] == ' ');
+				case 9:
+					return (Board[2][2] == ' ');
+				default:
+					return false;
+			}
+		}
 }
+	
+			
 
 
