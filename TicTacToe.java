43746d8ffa708b8b9f3fceb496982e7855d06a1a
sweetylenka <sweetylenka2013@gmail.com>
2021-03-31 13:06:30 +0530

    Ability to allow the player to choose a letter X or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 
 public class TicTacToe {
 	 
-	
 
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("WELCOME TO TIC-TAC-TOE GAME\n");
@@ -16,13 +15,11 @@ public class TicTacToe {
 		
 		
 		displayBoard(Board);
+		allowPlayerToChooseEitherXorO();
 		
-		
-		
-				
 		}
 
-	private static void displayBoard(char[][] Board) {
+	public static void displayBoard(char[][] Board) {
 		System.out.println(Board[0][0]+ "|" + Board[0][1]+ "|" +Board[0][2]);
 		System.out.println("-+-+-");
 		System.out.println(Board[1][0]+ "|" + Board[1][1]+ "|" +Board[1][2]);
@@ -30,9 +27,26 @@ public class TicTacToe {
 		System.out.println(Board[2][0]+ "|" + Board[2][1]+ "|" +Board[2][2]);
 	}
 				
-		
+	public static void allowPlayerToChooseEitherXorO()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose either X or O :");
+		String userInput = sc.nextLine();
+		char player1=userInput.charAt(0);
+		char computer;
+		if (player1 != 'X' && player1 != '0' && player1 != 'x' && player1 != 'o' )
+		{
+			System.out.println("Please enter a valid choice (Enter either X or O) : ");
+			 userInput = sc.nextLine();
+			 player1=userInput.charAt(0);
+		}	
+		if(player1 == 'X' || player1 == 'x')
+			computer ='O';
+		else
+			computer = 'X';
 		
 	}
-
+	
+}
 
 
