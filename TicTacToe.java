3c9c1b475f266a9ad17d22e59c0630a0e78372b5
SweetyLenka <55683708+SweetyLenka@users.noreply.github.com>
2021-04-01 08:46:50 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -17,24 +17,12 @@ public class TicTacToe {
 				  {' ',' ',' '},
 				  {' ',' ',' '}};
 		
-		displayBoard(Board);
+		
 		
 		
 		
 				
 		}
-	
-
-	private static void displayBoard(char[][] Board) {
-		System.out.println(Board[0][0]+ "|" + Board[0][1]+ "|" +Board[0][2]);
-		System.out.println("-+-+-");
-		System.out.println(Board[1][0]+ "|" + Board[1][1]+ "|" +Board[1][2]);
-		System.out.println("-+-+-");
-		System.out.println(Board[2][0]+ "|" + Board[2][1]+ "|" +Board[2][2]);
-	}
-				
-		
-		
 	}
 
 
