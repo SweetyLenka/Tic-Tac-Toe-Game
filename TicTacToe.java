67f89dfae06f10bb4c14b71d4b39eef22b907684
SweetyLenka <55683708+SweetyLenka@users.noreply.github.com>
2021-04-01 09:19:42 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -33,8 +33,6 @@ public class TicTacToe {
 		
 	}
 	
-	 
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -50,10 +48,6 @@ public class TicTacToe {
 		
 		}
 
-	
-	
-	
-	
 }
 
 
